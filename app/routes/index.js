7de2ae19cd40fd41b57f9bb3d05b6c6bff521e70
Leandro Calvo <lcalvo.intern@copyright.com>
2022-07-19 13:44:12 +0200

    hola

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,34 +1,39 @@
 import Route from '@ember/routing/route';
+//import {waitFor} from '@ember/test-helpers';
+
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 export default class IndexRoute extends Route {
 
-   
-/*     async model() {
-    return {
-      title: 'Grand Old Mansion',
-      owner: 'Veruca Salt',
-      city: 'San Francisco',
-      location: {
-        x: 3,
-        y: 2,
-        z: 4,
-      },
-      category: 'Estate',
-      type: 'Standalone',
-      bedrooms: 15,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-    }; 
-  } */
+/* Desde aquí hacemos el fetch de los datos... */
 
-      
- async model(){   
-
-    let response = await fetch('../api/rentals.json');
-    let parsed = await response.json();
-    console.log(parsed)
-    return parsed; 
-
-    }
+ async model(){ 
     
+      let response = await fetch('../api/rentals.json');
+      console.log("[STATUS]: " + response.status + ": " + response.statusText);
+      console.log("... Awaiting data");
+      let parsed = await response.json();
+      console.log(parsed);
+      await myFunction(1000);
+      console.log("... Awaiting [waitFor]")    
+      return parsed;
+      
+   
+  }
+
 }
+function myFunction(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// ================ ¿Qué es un promise? ================ 
+
+/**
+ * Una promesa (promise) es un objeto que representa la eventual finalización o fallo de una operación asíncrona y su valor resultante.
+ * 
+ * 1. ¡Prometo un resultado!
+ * 2. *producing the code* este código puede llevar algún tiempo...
+ * 3. *consumiendo código* este código debe esperar el resultado
+ * 
+ * Básicamente una promise es un objeto javascript que 
+ * 
+ **/
